f91819094d691b4a383c316e33b8fef46425b302
Francois Gaudin <contact@francoisgaudin.com>
2014-02-26 19:23:14 -0800

    Bounce version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='Embeder',
-    version='0.1.1',
+    version='0.1.2',
     author='Francois Gaudin',
     author_email='contact@francoisgaudin.com',
     packages=find_packages(),
